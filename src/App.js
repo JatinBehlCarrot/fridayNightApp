427f3a64716558b7a8a63ecbe6b15f46ae14413e
Shreya Sachdeva <sachdeva.shreya@gmail.com>
2021-07-23 15:03:08 -0700

    Added second and third question

--- src/App.js
+++ src/App.js
@@ -10,9 +10,13 @@ const App = () => {
     const quizQuestions = ["How are you feeling today?*", "Do you prefer indoors or outdoor?*", "Would you like to socialize or plan solo?*"];
     const quizOptions = [["Relaxed", "Cheery", "Snoozy", "Productive"], ["Outdoors", "Indoors"], ["Socialize", "Solo"]];
     const images = ["https://images.pexels.com/photos/3807758/pexels-photo-3807758.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "https://cdn.ramseysolutions.net/media/article-images/budget-date-night-l.jpg", "https://hips.hearstapps.com/clv.h-cdn.co/assets/18/02/1515470256-levi-guzman-268866.jpg"];
-
+    const [mood, setMood] = useState("");
+    const [location, setLocation] = useState("");
+    const [socialize, setSocialize] = useState("");
 
     const [firstQuesAnswered, setFirstQuesAnswered] = useState(false);
+    const [secondQuesAnswered, setSecondQuesAnswered] = useState(false);
+    const [thirdQuesAnswered, setThirdQuesAnswered] = useState(false);
 
 
     function handleStartQuiz() {
@@ -40,16 +44,44 @@ const App = () => {
             </div>
         )
     } else {
-        return (
-            <Question
-                quizQuestions={quizQuestions[0]}
-                quizOptions={quizOptions[0]}
-                images={images[0]}
-                setFirstQuesAnswered={setFirstQuesAnswered}
-            />
-        )
+        if (firstQuesAnswered === false) {
+            return (
+                <Question
+                    quizQuestions={quizQuestions[0]}
+                    quizOptions={quizOptions[0]}
+                    images={images[0]}
+                    setQuesAnswered={setFirstQuesAnswered}
+                    callBack={setMood}
+                    id="mood"
+                    choice={mood}
+                />
+            )
+        } else if (firstQuesAnswered) {
+            return (
+                <Question
+                    quizQuestions={quizQuestions[1]}
+                    quizOptions={quizOptions[1]}
+                    images={images[1]}
+                    setQuesAnswered={setSecondQuesAnswered}
+                    callBack={setLocation}
+                    id="location"
+                    choice={location}
+                />
+            )
+        } else if (secondQuesAnswered) {
+            return (
+                <Question
+                    quizQuestions={quizQuestions[2]}
+                    quizOptions={quizOptions[2]}
+                    images={images[2]}
+                    setQuesAnswered={setThirdQuesAnswered}
+                    callBack={setSocialize}
+                    id="friends"
+                    choice={socialize}
+                />
+            )
+        }
     }
-
 }
 
 ReactDOM.render(<StrictMode><App /></StrictMode>, document.getElementById("root"));
--- src/Question.js
+++ src/Question.js
@@ -1,41 +1,45 @@
 import React from "react";
 import { useState } from "react";
+import Header from "./Header";
 
-const Question = ({ quizQuestions, quizOptions, images, setFirstQuesAnswered }) => {
-    const [mood, setMood] = useState("");
+const Question = ({ quizQuestions, quizOptions, images, setQuesAnswered, callBack, id, choice }) => {
+
     return (
-        <div className="quesContainer">
-            <div className="quizQues">
-                <p> {quizQuestions} </p>
-            </div>
-            <div className="options" >
-                {
-                    quizOptions.map((emotion, index) =>
-                    (<>
-                        <input key={emotion}
-                            type="radio"
-                            id="mood"
-                            name="radioBtn"
-                            value={emotion}
-                            onChange={
-                                () => setMood(emotion)}
-                        />
-                        <label htmlFor="mood"
-                            key={index}
-                        > {emotion} </label>
-                    </>
-                    )
-                    )
-                }
-            </div>
-            <div className="mainImg" >
-                <img src={images} alt="image of a girl making all kinds of faces" />
-            </div>
-            <div className="quesBtnDiv" >
-                <button type="button"
-                    className="quesBtn"
-                    onClick={() => setFirstQuesAnswered(true)}> Next Question </button>
-            </div> </div>
+        <>
+            <Header />
+            <div className="quesContainer">
+                <div className="quizQues">
+                    <p> {quizQuestions} </p>
+                </div>
+                <div className="options" >
+                    {
+                        quizOptions.map((value, index) =>
+                        (<>
+                            <input key={value}
+                                type="radio"
+                                id={id}
+                                name="radioBtn"
+                                value={value}
+                                onChange={
+                                    () => { callBack(value); console.log(value) }}
+                            />
+                            <label htmlFor="mood"
+                                key={value.toUpperCase()}
+                            > {value} </label>
+                        </>
+                        )
+                        )
+                    }
+                </div>
+                <div className="mainImg" >
+                    <img src={images} />
+                </div>
+                <div className="quesBtnDiv" >
+                    <button type="button"
+                        className="quesBtn"
+                        onClick={() => { if (choice === "") { alert("Select a value from the given options!"); } else { setQuesAnswered(true); console.log(choice) }; }}> Next Question </button>
+                </div> </div>
+        </>
     )
 
 }
